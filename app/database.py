f46584c5247cc8bc67aca029f23366366ef2555f
Vibhu0412 <vbpurohit1948@gmail.com>
2025-10-12 16:48:24 +0530

    add print for db url

--- app/database.py
+++ app/database.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost/atcdrive")
+print("I AM DATABASE URL", DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
